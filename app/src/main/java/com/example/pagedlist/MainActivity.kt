4e8be5d6083d48f5e3aaee38b8e8d8547e946ea0
ngangavic <ngangavictor10@gmail.com>
2020-02-28 09:14:42 +0300

    main activity and internet permission

--- app/src/main/java/com/example/pagedlist/MainActivity.kt
+++ app/src/main/java/com/example/pagedlist/MainActivity.kt
@@ -2,11 +2,46 @@ package com.example.pagedlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: NewsListViewModel
+    private lateinit var newsListAdapter: NewsListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this)
+            .get(NewsListViewModel::class.java)
+        initAdapter()
+        initState()
     }
+
+    private fun initAdapter() {
+        newsListAdapter = NewsListAdapter { viewModel.retry() }
+        recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        recyclerView.adapter = newsListAdapter
+        viewModel.newsList.observe(this, Observer {
+            newsListAdapter.submitList(it)
+        })
+    }
+
+    private fun initState() {
+        textView.setOnClickListener { viewModel.retry() }
+        viewModel.getState().observe(this, Observer { state ->
+            progressBar.visibility = if (viewModel.listIsEmpty() && state == State.LOADING) View.VISIBLE else View.GONE
+            textView.visibility = if (viewModel.listIsEmpty() && state == State.ERROR) View.VISIBLE else View.GONE
+            if (!viewModel.listIsEmpty()) {
+                newsListAdapter.setState(state ?: State.DONE)
+            }
+        })
+    }
+
 }
