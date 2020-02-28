649470e85514911f0e175b39d86c208368cf5033
ngangavic <ngangavictor10@gmail.com>
2020-02-28 08:07:49 +0300

    list footer view holder

---
+++ app/src/main/java/com/example/pagedlist/ListFooterViewHolder.kt
@@ -0,0 +1,24 @@
+package com.example.pagedlist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_list_footer.view.*
+
+class ListFooterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(status: State?) {
+        itemView.progressBar.visibility = if (status == State.LOADING) VISIBLE else View.INVISIBLE
+        itemView.textView.visibility = if (status == State.ERROR) VISIBLE else View.INVISIBLE
+    }
+
+    companion object {
+        fun create(retry: () -> Unit, parent: ViewGroup): ListFooterViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list_footer, parent, false)
+            view.textView.setOnClickListener { retry() }
+            return ListFooterViewHolder(view)
+        }
+    }
+}
